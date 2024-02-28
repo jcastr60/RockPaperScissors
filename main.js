d56c27ace33fa34be274b6e1b9c238a8836bdd14
jcastr60 <jasminecastro016@gmail.com>
2024-02-27 22:47:27 -0600

    Got function getCommputerChoice to work which allows for the computer to make a choice between rock,paper,scissors

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-// function getComputerChoice(){
+// Get the computers choice for Rock Paper Scissors
+function getComputerChoice(){
+    // have options
+    let options = ['rock', 'paper','scissor'];
+    
+    // randomize the choice based on options list
+    let choice = Math.random() * (4 - 1) + 1;
+    // return that option
+    return options[(Math.floor(choice)) - 1];
+}
 
-// }
+console.log(getComputerChoice());
