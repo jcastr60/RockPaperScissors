dcddf86d91e4b7e35ecc065d40c3c445d08f4eaa
jcastr60 <jasminecastro016@gmail.com>
2024-02-28 00:33:39 -0600

    Added playGame function to allow the game to gon on for 5 rounds.

--- main.js
+++ main.js
@@ -12,29 +12,45 @@ function getComputerChoice(){
 function playRound(ComputerSelection, playerSelection){
     if (playerSelection == "rock" && ComputerSelection ==  "scissor") {
         return "You Won! Rock beats Scissor";
+        // playerWins++;
         
     } else if (playerSelection == "paper" && ComputerSelection ==  "rock"){
         return "You Won! Paper beats Rock";
+        // playerWins++;
     }  else if (playerSelection == "scissor" && ComputerSelection ==  "paper"){
         return "You Won! Scissor beats Rock";
+        // playerWins++;
     } else {
         let beats = playerSelection.concat(" beats ", ComputerSelection);
         let lostMessage = "You Lost! "
+        // computerWins++;
         return lostMessage.concat(beats);
     }
 }
 
-// function playGame(){
-//     // Count number of wins
-//     let computerWins = 0;
-//     let player
+function playGame(){
+    // // Count number of wins
+    // let computerWins = 0;
+    // let playerWins = 0;
 
+    // var name = prompt("Rock, Paper, or Scissor?");
+    // // loop to keep playing game until one openet has 5 wins
+    // while ((playerWins < 5) || (computerWins < 5)) {
+    //     if (name != "rock" || name != "paper" || name != "scissor"){
+    //         name = prompt("Rock, Paper, or Scissor?");
+    //     } else {
+    //         playerWins = name;
+    //         playRound(getComputerChoice(), playerSelection, computerWins, playerWins);
+    //     }
+    // }
 
-//     // loop to keep playing game until one openet has 5 wins
+    console.log(playRound(getComputerChoice(), "rock"));
+    console.log(playRound(getComputerChoice(),"paper"));
+    console.log(playRound(getComputerChoice(), "scissors"));
+    console.log(playRound(getComputerChoice(),"rock"));
+    console.log(playRound(getComputerChoice(), "paper"));
+}
 
-
-
-// }
-
-console.log(getComputerChoice());
-console.log(playRound("rock", "paper"));
+// console.log(getComputerChoice());
+// console.log(playRound("rock", "paper"));
+console.log(playGame());
