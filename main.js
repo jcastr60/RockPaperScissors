2bd8c1ba11a7dc22787b31183fda4b4979143514
jcastr60 <jasminecastro016@gmail.com>
2024-02-28 00:09:59 -0600

    Added function play round that allows for the game of rock paper scissors to be played once

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Get the computers choice for Rock Paper Scissors
 function getComputerChoice(){
     // have options
-    let options = ['rock', 'paper','scissor'];
+    let options = ["rock", "paper","scissor"];
     
     // randomize the choice based on options list
     let choice = Math.random() * (4 - 1) + 1;
@@ -9,4 +9,32 @@ function getComputerChoice(){
     return options[(Math.floor(choice)) - 1];
 }
 
+function playRound(ComputerSelection, playerSelection){
+    if (playerSelection == "rock" && ComputerSelection ==  "scissor") {
+        return "You Won! Rock beats Scissor";
+        
+    } else if (playerSelection == "paper" && ComputerSelection ==  "rock"){
+        return "You Won! Paper beats Rock";
+    }  else if (playerSelection == "scissor" && ComputerSelection ==  "paper"){
+        return "You Won! Scissor beats Rock";
+    } else {
+        let beats = playerSelection.concat(" beats ", ComputerSelection);
+        let lostMessage = "You Lost! "
+        return lostMessage.concat(beats);
+    }
+}
+
+// function playGame(){
+//     // Count number of wins
+//     let computerWins = 0;
+//     let player
+
+
+//     // loop to keep playing game until one openet has 5 wins
+
+
+
+// }
+
 console.log(getComputerChoice());
+console.log(playRound("rock", "paper"));
